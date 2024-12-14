298acbfee23474fe27828a3ef4011e0c6910b4d5
Admin <Admin@DESKTOP-E6HB3SG>
2024-12-14 11:22:19 +0530

    Dev2 get commit

--- ProductApp/src/main/java/com/cjc/main/controller/HomeController.java
+++ ProductApp/src/main/java/com/cjc/main/controller/HomeController.java
@@ -1,8 +1,17 @@
 package com.cjc.main.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+	@GetMapping("/get-products")
+	public List allProducts()
+	{
+		return new ArrayList<>();
+	}
 }
